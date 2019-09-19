ab723f7cf315fd3edba8a1ea9801dd7d8af6063b
nireo <eemil.lehtikangas1@gmail.com>
2019-09-19 21:13:52 +0300

    added registeration functionality

--- frontend/src/components/Login/CreateAccount.tsx
+++ frontend/src/components/Login/CreateAccount.tsx
@@ -1,25 +1,25 @@
-import React, { useState } from 'react';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import NavBar from '../layout/NavBar';
-import Copyright from '../Copyright';
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Link from "@material-ui/core/Link";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import NavBar from "../layout/NavBar";
+import Copyright from "../Copyright";
+import userService from "../../services/users";
+import CreateUser from "../../interfaces/user.interface";
 
 const useStyles = makeStyles(theme => ({
   layout: {
-    width: 'auto',
+    width: "auto",
     marginLeft: theme.spacing(2),
     marginRight: theme.spacing(2),
     [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
       width: 600,
-      marginLeft: 'auto',
-      marginRight: 'auto'
+      marginLeft: "auto",
+      marginRight: "auto"
     }
   },
   paper: {
@@ -36,30 +36,46 @@ const useStyles = makeStyles(theme => ({
     padding: theme.spacing(3, 0, 5)
   },
   buttons: {
-    display: 'flex',
-    justifyContent: 'flex-end'
+    display: "flex",
+    justifyContent: "flex-end"
   },
   button: {
     marginTop: theme.spacing(3),
     marginLeft: theme.spacing(1)
   },
   form: {
-    width: '100%',
+    width: "100%",
     marginTop: theme.spacing(1)
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
-    width: '100%'
+    width: "100%"
   }
 }));
 
 const CreateAccount: React.FC = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [email, setEmail] = useState('');
-  const [confirmEmail, setConfirmEmail] = useState('');
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
+  const [email, setEmail] = useState("");
+  const [confirmEmail, setConfirmEmail] = useState("");
   const classes = useStyles();
+
+  const createAccount = async (): Promise<void> => {
+    if (confirmEmail === email && confirmPassword === password) {
+      if (
+        window.confirm("Are you sure you want to create user named " + username)
+      ) {
+        const newUserObject: CreateUser = {
+          username,
+          password,
+          email
+        };
+        await userService.registerUser(newUserObject);
+      }
+    }
+  };
+
   return (
     <div>
       <CssBaseline />
@@ -69,7 +85,7 @@ const CreateAccount: React.FC = () => {
           <Typography component="h1" variant="h4" align="center">
             Create User
           </Typography>
-          <form className={classes.form} noValidate>
+          <form className={classes.form} noValidate onSubmit={createAccount}>
             <TextField
               variant="outlined"
               margin="normal"
--- frontend/src/components/Posts/private/CreatePost.tsx
+++ frontend/src/components/Posts/private/CreatePost.tsx
@@ -1,26 +1,22 @@
-import React, { useState } from 'react';
-import Button from '@material-ui/core/Button';
-import CssBaseline from '@material-ui/core/CssBaseline';
-import TextField from '@material-ui/core/TextField';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import Checkbox from '@material-ui/core/Checkbox';
-import Link from '@material-ui/core/Link';
-import Paper from '@material-ui/core/Paper';
-import Grid from '@material-ui/core/Grid';
-import Typography from '@material-ui/core/Typography';
-import { makeStyles } from '@material-ui/core/styles';
-import NavBar from '../../layout/NavBar';
-import Copyright from '../../Copyright';
+import React, { useState } from "react";
+import Button from "@material-ui/core/Button";
+import CssBaseline from "@material-ui/core/CssBaseline";
+import TextField from "@material-ui/core/TextField";
+import Paper from "@material-ui/core/Paper";
+import Typography from "@material-ui/core/Typography";
+import { makeStyles } from "@material-ui/core/styles";
+import NavBar from "../../layout/NavBar";
+import Copyright from "../../Copyright";
 
 const useStyles = makeStyles(theme => ({
   layout: {
-    width: 'auto',
+    width: "auto",
     marginLeft: theme.spacing(2),
     marginRight: theme.spacing(2),
     [theme.breakpoints.up(600 + theme.spacing(2) * 2)]: {
       width: 600,
-      marginLeft: 'auto',
-      marginRight: 'auto'
+      marginLeft: "auto",
+      marginRight: "auto"
     }
   },
   paper: {
@@ -37,26 +33,26 @@ const useStyles = makeStyles(theme => ({
     padding: theme.spacing(3, 0, 5)
   },
   buttons: {
-    display: 'flex',
-    justifyContent: 'flex-end'
+    display: "flex",
+    justifyContent: "flex-end"
   },
   button: {
     marginTop: theme.spacing(3),
     marginLeft: theme.spacing(1)
   },
   form: {
-    width: '100%',
+    width: "100%",
     marginTop: theme.spacing(1)
   },
   submit: {
     margin: theme.spacing(3, 0, 2),
-    width: '100%'
+    width: "100%"
   }
 }));
 
 const CreatePost: React.FC = () => {
-  const [title, setTitle] = useState('');
-  const [content, setContent] = useState('');
+  const [title, setTitle] = useState("");
+  const [content, setContent] = useState("");
   const classes = useStyles();
   return (
     <div>
