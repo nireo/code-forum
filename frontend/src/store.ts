02a15344e52bcd4085ede46a6ff4097056fe55d2
nireo <eemil.lehtikangas1@gmail.com>
2019-10-23 16:17:12 +0300

    removed the ownposts reducer

--- frontend/src/store.ts
+++ frontend/src/store.ts
@@ -3,14 +3,12 @@ import thunk from "redux-thunk";
 import userReducer from "./reducers/userReducer";
 import postReducer from "./reducers/postReducer";
 import usersReducer from "./reducers/usersReducer";
-import ownPostsReducer from "./reducers/ownPosts";
 import notificationReducer from "./reducers/notification";
 
 const reducer = combineReducers({
   user: userReducer,
   posts: postReducer,
   users: usersReducer,
-  ownPosts: ownPostsReducer,
   notification: notificationReducer
 });
 
